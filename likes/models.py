055e2531e849901b682d24413b94f5a593eff513
rdonii <rdonii@mail.ru>
2022-06-08 00:09:40 -0700

    likes added

--- likes/models.py
+++ likes/models.py
@@ -1,13 +1,10 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.auth.models import User
 
-class Tag(models.Model):
-    label = models.CharField(max_length=255)
-
-class TagItem(models.Model):
-    tag = models.ForeignKey(to=Tag, on_delete=models.CASCADE)
-
+class LikedItem(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     content_type = models.ForeignKey(to=ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey()
