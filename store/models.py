6a47f15c585df2cdc24b755b261393f22997a607
rdonii <rdonii@mail.ru>
2022-06-08 04:09:12 -0700

    MySQL setup

--- store/models.py
+++ store/models.py
@@ -6,12 +6,13 @@ class Promotion(models.Model):
 
 class Collection(models.Model):
     title = models.CharField(max_length=255)
-    featured_product = models.ForeignKey(to='Product', on_delete=models.SET_NULL, related_name='+')
+    featured_product = models.ForeignKey(to='Product', on_delete=models.SET_NULL, null=True, related_name='+')
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(to=Collection, on_delete=models.PROTECT)
