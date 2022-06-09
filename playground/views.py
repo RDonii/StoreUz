756a123e540978bf6ed6eec53d50f605d9af3191
rdonii <rdonii@mail.ru>
2022-06-09 03:50:39 -0700

    generic reletionship view

--- playground/views.py
+++ playground/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render
-from store.models import Customer, Collection, Product, Order, OrderItem
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product, Collection, Customer, Address, Promotion, Cart, CartItem
+from tags.models import TaggedItem
+import random
 
 def homeview(request):
-    customers = Customer.objects.filter(email__icontains='.com')
-    collections = Collection.objects.filter(featured_product__isnull=True)
-    products = Product.objects.filter(inventory__lt=10)
-    orders = Order.objects.filter(customer__id=1)
-    orderItems = OrderItem.objects.filter(product__collection__id=3)
-
-    context = {
-        "customers": customers,
-        "collections": collections,
-        "products": products,
-        "orders": orders,
-        "orderItems": orderItems,
-    }
-    print(context)
-
-    return render(request, 'index.html', context=context)
+    obj_type = random.choice([Product, Collection, Customer, Address, Promotion, Cart, CartItem])
+    result = TaggedItem.objects.get_tags_for(obj_type, random.randint(0, 1000))
+    
+    return render(request, 'index.html', {'result': result})
