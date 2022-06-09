7a3e6658bb285bbc4bb80a582689c8c87944f935
rdonii <rdonii@mail.ru>
2022-06-08 21:14:46 -0700

    filtering play

--- playground/views.py
+++ playground/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from store.models import Customer, Collection, Product, Order, OrderItem
 
 def homeview(request):
-    return render(request, 'index.html')
+    customers = Customer.objects.filter(email__icontains='.com')
+    collections = Collection.objects.filter(featured_product__isnull=True)
+    products = Product.objects.filter(inventory__lt=10)
+    orders = Order.objects.filter(customer__id=1)
+    orderItems = OrderItem.objects.filter(product__collection__id=3)
+
+    context = {
+        "customers": customers,
+        "collections": collections,
+        "products": products,
+        "orders": orders,
+        "orderItems": orderItems,
+    }
+    print(context)
+
+    return render(request, 'index.html', context=context)
