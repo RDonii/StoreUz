c47c750345f7465aaa99c4072f66d5b2051941a0
RDonii <RDonii@mail.ru>
2022-07-13 11:58:15 +0500

    3.2.7

--- storefront/settings.py
+++ storefront/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'djoser',
     'playground',
     'debug_toolbar',
+    "corsheaders",
     'store',
     'tags',
     'likes',
@@ -54,11 +55,17 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8001',
+    'http://localhost:8001',
 ]
 
 INTERNAL_IPS = [
